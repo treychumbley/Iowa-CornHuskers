42a571e7b9590c1a8c522e026bfa997e5faca545
Trey Chumbley <trey.chumbley@my.simpson.edu>
2018-10-23 09:22:48 -0500

    Create Nav and Footer php

---
+++ resources/footer.php
@@ -0,0 +1,6 @@
+<!-- Footer -->
+<div class="jumbotronfooter">
+<img alt="EBG pic" src="../public_html/img/EBGpic_assignment1.png" />
+<footer>
+    &copy; Copyright 2018, Sam Adeniyi, Katelyn Mangold, Trey Chumbley
+</footer>
---
+++ resources/navigation.php
@@ -0,0 +1,39 @@
+<!-- Navigation Bar -->
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="index.html">Iowa Cornhuskers</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="           false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="roster.html">Roster</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="schedule.html">Schedule</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="coaches.html">Coaches</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="news.html">News</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="sign_up.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Sign Up
+            </a>
+             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="sign_up.html">Sign Up</a>
+              <a class="dropdown-item" href="login.html">Login</a>
+            </div>
+          </li>
+        </ul>
+
+      </div>
+    </nav>
