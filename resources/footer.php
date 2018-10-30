8f276b2e71e2fd0cdc7e79650b0117b35538f4bc
Trey Chumbley <trey.chumbley@my.simpson.edu>
2018-10-30 09:18:52 -0500

    Fix footer

--- resources/footer.php
+++ resources/footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <div class="jumbotronfooter">
-<img alt="EBG pic" src="../public_html/img/EBGpic_assignment1.png" />
+<img alt="EBG pic" src="img/EBGpic_assignment1.png" />
 <footer>
     &copy; Copyright 2018, Sam Adeniyi, Katelyn Mangold, Trey Chumbley
 </footer>
